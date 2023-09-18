2f3ab98cf937ebd5390de487f5b03c04dc177b86
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-18 12:11:10 +0200

    b

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-from .user import User
-from .db import environment, SCHEMA
-from .articles import Article
-from .article_comments import Comment
-from .article_likes import ArticleLike
-# from .follows import Follow
-from .user_reading_list import ReadingList
