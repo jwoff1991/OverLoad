dd621c6007e05c040aed64ad3f66f54a33464d95
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-09-24 17:48:02 +0200

    fixing null error on render

--- react-app/src/components/Articles/ArticleLikes/index.js
+++ react-app/src/components/Articles/ArticleLikes/index.js
@@ -69,7 +69,7 @@ const ArticleLikes = (sessionUser, likes, articleId) => {
 
   //choses which button to display based on if user is in artile like list or not
   const likeButton = () => {
-    if (userIdFromLikes.includes(sessionUser.id)) {
+    if (userIdFromLikes && userIdFromLikes.includes(sessionUser.id)) {
       return likeButtonBlack;
     } else {
       return likeButtonClear;
