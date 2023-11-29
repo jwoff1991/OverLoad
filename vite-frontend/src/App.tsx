12a7d8d75d5781bb7841da9da07ae482ab6c9b1b
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-11-29 09:21:06 +0100

    changed template, corrcted main, css and app. commented out everything not build yet

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactGA from 'react-ga';
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+const TRACKING_ID = "G-CEEJHMT4H8"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    // dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Navigation isLoaded={isLoaded} /> */}
+      {isLoaded && (
+        <Routes>
+          <Route path="/login" >
+            {/* <LoginFormPage /> */}
+          </Route>
+          <Route path="/signup">
+            {/* <SignupFormPage /> */}
+          </Route>
+          <Route path='/'>
+            {/* <ArticlesComponent /> */}
+          </Route>
+          <Route path='/articles/:id' >
+            {/* <SingleArticle /> */}
+          </Route>
+            <Route path='/new-article' >
+            {/* <CreateNewArticle /> */}
+            </Route>
+            <Route path='/article/:id/edit' >
+              {/* <EditArticle /> */}
+            </Route>
+            <Route path='/:userid/reading-list' >
+              {/* <ReadingListComponent /> */}
+            </Route>
+            <Route path='/my-story' >
+              {/* <MyStoryComponent /> */}
+            </Route>
+            <Route path='/create' >
+              {/* <CreateComponent /> */}
+            </Route>
+        </Routes>
+      )}
     </>
   )
 }
--- vite-frontend/src/main.tsx
+++ vite-frontend/src/main.tsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+	window.sessionActions = sessionActions;
+}
+
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+          <App />
+  </React.StrictMode>
+);
+
+//once these are created you can place back into the root
+    // <ModalProvider>
+    //   <Provider store={store}>
+    //     <BrowserRouter>
+    //       <Modal />
+    //     </BrowserRouter>
+    //   </Provider>
+    // </ModalProvider>
