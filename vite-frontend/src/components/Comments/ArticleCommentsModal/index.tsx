caac9488fa38f53c827b8cc41461f219c072b811
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-02-07 11:44:40 +0100

    added text to comment box if there are no comments

--- vite-frontend/src/components/Comments/ArticleCommentsModal/index.tsx
+++ vite-frontend/src/components/Comments/ArticleCommentsModal/index.tsx
@@ -63,79 +63,84 @@ function CommentsModal(props: { articleId: number }) {
       <div className="comments-responses-div">
         <h3>Responses ({comments.length ? comments.length : 0})</h3>
       </div>
+      {comments.length == 0 ?
+      <div className="no-comments-yet">This article has no comments</div>
+      : null}
       {sessionUser && sessionUser.id && (
-        <div className="comments-create-div-with-buttons">
-          <div className="comments-create">
-            <textarea
-              className="post-comment-form"
-              placeholder="What are your thoughts?"
-              maxLength={255}
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            ></textarea>
-            <div className="comment-create-buttons">
-              <div className="comment-form-buttons">
-                <button
-                  className="cancle-button-comment-form"
-                  onClick={resetComment}
-                >
-                  Cancel
-                </button>
-              </div>
-              <div className="comment-form-buttons">
-                <button
-                  className="submit-new-comment-button"
-                  onClick={handleSubmit}
-                  disabled={isDisabled}
-                >
-                  Respond
-                </button>
+          <div className="comments-create-div-with-buttons">
+            <div className="comments-create">
+              <textarea
+                className="post-comment-form"
+                placeholder="What are your thoughts?"
+                maxLength={255}
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              ></textarea>
+              <div className="comment-create-buttons">
+                <div className="comment-form-buttons">
+                  <button
+                    className="cancle-button-comment-form"
+                    onClick={resetComment}
+                  >
+                    Cancel
+                  </button>
+                </div>
+                <div className="comment-form-buttons">
+                  <button
+                    className="submit-new-comment-button"
+                    onClick={handleSubmit}
+                    disabled={isDisabled}
+                  >
+                    Respond
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-      <div className="comments-display">
-        {comments &&
-          comments.map(({ commenter, body, id, date_created }) => (
-            <div key={id} className="comments-container">
-              <div className="commentoer-name-date-edit-delete">
-                <div className="commentor-name-post-date">
-                  <div className="commentor-name">
-                    {commenter.firstname}
-                    <span>&#183;</span>
-                    <div className="comment-post-date">
-                      {dateChanger(date_created)}
+        )}
+        <div className="comments-display">
+          {comments &&
+            comments.map(({ commenter, body, id, date_created }) => (
+              <div key={id} className="comments-container">
+                <div className="commentoer-name-date-edit-delete">
+                  <div className="commentor-name-post-date">
+                    <div className="commentor-name">
+                      {commenter.firstname}
+                      <span>&#183;</span>
+                      <div className="comment-post-date">
+                        {dateChanger(date_created)}
+                      </div>
                     </div>
                   </div>
+                  <div className="edit-delete-buttons-comments">
+                    {sessionUser &&
+                      sessionUser.id &&
+                      sessionUser.id === commenter.id && (
+                        <>
+                          <OpenModal
+                            buttonText="Edit"
+                            modalComponent={
+                              <EditCommentModal props={[id, body, sessionUser]} />
+                            }
+                            className="comment-edit-button"
+                          />
+                          <OpenModal
+                            buttonText="Delete"
+                            modalComponent={
+                              <DeleteCommentModal props={[id, articleId]} />
+                            }
+                            className="comment-delete-button"
+                          />
+                        </>
+                      )}
+                  </div>
                 </div>
-                <div className="edit-delete-buttons-comments">
-                  {sessionUser &&
-                    sessionUser.id &&
-                    sessionUser.id === commenter.id && (
-                      <>
-                        <OpenModal
-                          buttonText="Edit"
-                          modalComponent={
-                            <EditCommentModal props={[id, body, sessionUser]} />
-                          }
-                          className="comment-edit-button"
-                        />
-                        <OpenModal
-                          buttonText="Delete"
-                          modalComponent={
-                            <DeleteCommentModal props={[id, articleId]} />
-                          }
-                          className="comment-delete-button"
-                        />
-                      </>
-                    )}
-                </div>
+                <div className="comment-body">{body}</div>
               </div>
-              <div className="comment-body">{body}</div>
-            </div>
-          ))}
-      </div>
+            ))}
+
+        </div>
+
     </div>
   );
 }
