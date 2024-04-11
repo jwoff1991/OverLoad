f7a797f478168aabb9d0930b8e4c2dacbb9a98d6
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-04-11 19:16:47 +0200

    fixing types

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="footer-fiv">
 
         <Link to='/my-story'>Story</Link>
-        <a href='https://github.com/jwoff1991' target='_blank' rel="noreferrer">Gitub</a>
+        <a href='https://github.com/jwoff1991' target='_blank' rel="noreferrer">GitHub</a>
         <a href='https://www.linkedin.com/in/jonathanbwofford/' target='_blank' rel="noreferrer">LinkedIn</a>
         <a href='https://myportfolio-jonathanwofford.netlify.app/' target='_blank' rel="noreferrer">Portfolio</a>
         </div>
