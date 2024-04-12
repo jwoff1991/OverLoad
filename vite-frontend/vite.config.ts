8ac4cee0d1ebe835e61bc01ca74a73f48d826f2b
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-04-12 09:38:47 +0200

    debugging render build errors

--- vite-frontend/vite.config.ts
+++ vite-frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // The output directory for build files
+    assetsDir: 'assets', // The directory under outDir to place assets in
+    
+  },
   server: {
     proxy: {
         '/api': {
