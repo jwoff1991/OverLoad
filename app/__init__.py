bb4a129bf7dd670f2b2451fc91aec104dc37773e
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-04-12 09:35:14 +0200

    debugging render build errors

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from .config import Config
 from .api.article_routes import article_routes
 from werkzeug.exceptions import NotFound
 
-app = Flask(__name__, static_folder='disc')
+app = Flask(__name__, static_folder='dist')
 
 # Setup login manager
 login = LoginManager(app)
