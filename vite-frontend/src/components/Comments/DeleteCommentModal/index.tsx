e634eb4f0391d75e90acbc6660f295cac0406b3b
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-04-16 11:35:41 +0200

    canceling delete comment does not force reload

--- vite-frontend/src/components/Comments/DeleteCommentModal/index.tsx
+++ vite-frontend/src/components/Comments/DeleteCommentModal/index.tsx
@@ -25,7 +25,6 @@ function DeleteCommentModal(props: { props: [commentId: number, articleId: numbe
   const returnToComments = (e: FormEvent) => {
     e.preventDefault();
     closeModal();
-    window.location.reload();
   };
 
   return (
