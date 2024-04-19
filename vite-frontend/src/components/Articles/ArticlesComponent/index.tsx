4d884da125f44d258950ce4ceb104c3c62b3d74c
Jonathan Wofford <jonathanbrodie91@gmail.com>
2024-04-19 12:13:32 +0200

    added a memorized selector to reduce redundency in function calls

--- vite-frontend/src/components/Articles/ArticlesComponent/index.tsx
+++ vite-frontend/src/components/Articles/ArticlesComponent/index.tsx
@@ -13,97 +13,110 @@ import ReadingListButton from "../../readingListButton";
 
 import './articlesComponent.css'
 
+import { createSelector } from "reselect";
 const ArticlesComponent = () => {
-    const dispatch: AppDispatch = useDispatch<AppDispatch>();
-    const sessionUser = useSelector((state: StateType) => state.session.user);
-    const articlesList = useSelector((state: StateType) =>
-      Object.values(state.articles.allArticles)
-    );
+  const dispatch: AppDispatch = useDispatch<AppDispatch>();
+  const sessionUser = useSelector((state: StateType) => state.session.user);
+  const selectAllArticles = (state: StateType) => state.articles.allArticles;
 
-    useEffect(() => {
-      dispatch(getAllArticles());
-      if (sessionUser && sessionUser.id) {
-        dispatch(clearUserReadingList());
-        dispatch(getUserReadingList());
-      }
-    }, [dispatch, sessionUser]);
+  // Memoized selector that returns the same reference if the underlying articles haven't changed
+  const selectArticlesList = createSelector(
+    [selectAllArticles],
+    (allArticles) => Object.values(allArticles)
+  );
 
-    const ArticleLink = ({ id, author, title, body, date_created }: ArticleType) => (
-      <NavLink key={id} to={`/articles/${id}`} className="text-link">
-        <div className="full-article-div">
-          <div className="date-author-reading-list-div">
-            <div className="date-article-author">
-              <div className="author">
-                {author.firstname} {author.lastname}
-              </div>
-              <span>&#183;</span>
-              <div className="date-created">
-                {articleDateConverter(date_created)}
-              </div>
+  const articlesList = useSelector(selectArticlesList);
+
+  useEffect(() => {
+    dispatch(getAllArticles());
+    if (sessionUser && sessionUser.id) {
+      dispatch(clearUserReadingList());
+      dispatch(getUserReadingList());
+    }
+  }, [dispatch, sessionUser]);
+
+  const ArticleLink = ({
+    id,
+    author,
+    title,
+    body,
+    date_created,
+  }: ArticleType) => (
+    <NavLink key={id} to={`/articles/${id}`} className="text-link">
+      <div className="full-article-div">
+        <div className="date-author-reading-list-div">
+          <div className="date-article-author">
+            <div className="author">
+              {author.firstname} {author.lastname}
             </div>
-            {sessionUser && <ReadingListButton articleId={id} />}
+            <span>&#183;</span>
+            <div className="date-created">
+              {articleDateConverter(date_created)}
+            </div>
           </div>
-          <div className="article-title">{title}</div>
-          <div className="article-body">{articleBodyConverter(body)}</div>
+          {sessionUser && <ReadingListButton articleId={id} />}
         </div>
-      </NavLink>
-    );
+        <div className="article-title">{title}</div>
+        <div className="article-body">{articleBodyConverter(body)}</div>
+      </div>
+    </NavLink>
+  );
 
-    return (
-      <>
-        {sessionUser ? (
-          <>
-            <div className="articles-topics-footer-container">
-              <div className="articles-container">
-                {articlesList.map((article) => (
-                  <ArticleLink {...article} />
-                ))}
-              </div>
-              <div className="topics-footer-container-user-logged-in">
-                <div className="staff-picks-user-logged-in">
-                  <StaffPicks />
-                  <Footer />
-                </div>
+  return (
+    <>
+      {sessionUser ? (
+        <>
+          <div className="articles-topics-footer-container">
+            <div className="articles-container">
+              {articlesList.map((article) => (
+                <ArticleLink key={article.id} {...article} />
+              ))}
+            </div>
+            <div className="topics-footer-container-user-logged-in">
+              <div className="staff-picks-user-logged-in">
+                <StaffPicks />
+                <Footer />
               </div>
             </div>
-          </>
-        ) : (
-          <>
-            <div className="user-not-logged-in-div">
-              <div className="div-next-to-image">
-                <h1>Welcome to OverLoad</h1>
-                <h4>(a Medium.com clone)</h4>
-                <p>
-                  Feel free to navigate the site, you can create your own
-                  signin, or login as a demo user. You can also, create an
-                  article, or comment on one you find interesting! If you have
-                  any questions or want to find out more about the sourcecode,
-                  visit the My Story link!
-                </p>
-              </div>
-              <img
-                className="image-for-front-page"
-                src="/icons/frontpageimage.png"
-                alt="blue honeycomb"
-              />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="user-not-logged-in-div">
+            <div className="div-next-to-image">
+              <h1>Welcome to OverLoad</h1>
+              <h4>(a Medium.com clone)</h4>
+              <p>
+                Feel free to navigate the site, you can create your own signin,
+                or login as a demo user. You can also, create an article, or
+                comment on one you find interesting! If you have any questions
+                or want to find out more about the sourcecode, visit the My
+                Story link!
+              </p>
             </div>
-            <div className="articles-topics-footer-container">
-              <div className="articles-container">
-                {articlesList.map((article) => (
-                  <ArticleLink {...article} />
-                ))}
-              </div>
-              <div className="topics-footer-container-user-not-logged-in">
-                <div className="staff-picks-user-not-logged-in">
-                  <StaffPicks />
-                  <Footer />
-                </div>
+            <img
+              className="image-for-front-page"
+              src="/icons/frontpageimage.png"
+              alt="blue honeycomb"
+            />
+          </div>
+          <div className="articles-topics-footer-container">
+            <div className="articles-container">
+              {articlesList.map((article) => (
+                <ArticleLink key={article.id} {...article} />
+              ))}
+            </div>
+            <div className="topics-footer-container-user-not-logged-in">
+              <div className="staff-picks-user-not-logged-in">
+                <StaffPicks />
+                <Footer />
               </div>
             </div>
-          </>
-        )}
-      </>
-    );
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ArticlesComponent;
